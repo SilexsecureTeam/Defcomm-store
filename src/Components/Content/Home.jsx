ab26c41fc10f013783d971198fab6453f5192b64
SilexsecureTeam <121134640+SilexsecureTeam@users.noreply.github.com>
2025-07-16 00:21:47 +0100

    Update Home.jsx

--- src/Components/Content/Home.jsx
+++ src/Components/Content/Home.jsx
@@ -110,7 +110,7 @@ function Home({ slideShow = true }) {
           <div className="flex py-4 md:py-8 gap-4 text-black">
             <button
               className="py-2 px-5 bg-white rounded-md"
-              onClick={() => navigate("/register")}
+              onClick={() => navigate("/membership/individual")}
             >
               GET STARTED
             </button>
