19ffb97ae4c6f7a6b99f98c2415b1fc03231e195
SilexsecureTeam <121134640+SilexsecureTeam@users.noreply.github.com>
2025-07-15 13:40:39 +0100

    Update HeaderBarTwo.jsx

--- src/Components/store/HeaderBarTwo.jsx
+++ src/Components/store/HeaderBarTwo.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 const HeaderBarTwo = ({ title = "", link = "" }) => {
   const navigate = useNavigate();
   return (
-    <div className="flex justify-between items-center py-3 pb-5">
+    <div className="flex justify-between items-center py-3 pb-5 max-w-[1200px] mx-auto">
       <h1 className="text-white text-2xl md:text-3xl font-semibold">
         {title || " "}
       </h1>
